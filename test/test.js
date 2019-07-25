c46589d5c8dfce7a0d3dcfa9b9adab2c9de83d05
David Hunt <git@dnotes.net>
2019-07-25 15:49:22 -0700

    Updated tests with major changes

--- test/test.js
+++ test/test.js
@@ -6,6 +6,10 @@ var generate = require('markdown-it-testgen')
 /*eslint-env mocha*/
 
 describe('Tests for markdown-it plugin', function () {
-  var md = require('markdown-it')().use(require('../'))
+  var md = require('markdown-it')()
+    .use(require('../'))
+    .use(require('markdown-it-attrs'), {
+      allowedAttributes: ['class'],
+    })
   generate(path.join(__dirname, 'fixtures/definitions.txt'), { header: true }, md)
 })
